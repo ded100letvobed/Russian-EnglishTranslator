e2b63dfb31fa08ea74b8b5946578a0ee84b53c40
User <User@DESKTOP-442GTNQ>
2017-08-05 09:02:57 +0300

    Добавьте файлы проекта.

---
+++ Russian-EnglishTranslator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Russian_EnglishTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
